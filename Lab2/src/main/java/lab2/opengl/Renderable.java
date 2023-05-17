623b05d87daf6c2b765c7b2f1931f77a1cbc52dd
Кравченко Валентин <kravavalik02@gmail.com>
2023-05-17 10:51:38 +0300

    Added Lab2

---
+++ Lab2/src/main/java/lab2/opengl/Renderable.java
@@ -0,0 +1,13 @@
+package lab2.opengl;
+
+/**
+ * Callback for OpenGL render operation
+ *
+ * @author Viktor Gubin
+ */
+public interface Renderable {
+
+    void render(int width, int height);
+
+}
+
